951e96689996c7d8ba53805d9619a8c57f7dbbdd
nicanico <nicsousan@gmail.com>
2023-02-11 12:55:11 -0300

    update 11/02 -  exercício média finalizado

--- exercicio-media/app.js
+++ exercicio-media/app.js
@@ -17,66 +17,74 @@ entradaDados.question('Informe o nome do aluno(a): ', function (nomeAluno) {
     let nomeAlun = nomeAluno
 
     entradaDados.question('Informe o sexo do aluno(Digite F ou M): ', function (sexoAlun) {
-        let generoAlun = sexoAlun
+        let generoAlun = sexoAlun.toUpperCase()
 
-        entradaDados.question('Informa nome do professor(a): ', function (nomeProf){
+        entradaDados.question('Informa nome do professor(a): ', function (nomeProf) {
             let nomeProfessor = nomeProf
 
             entradaDados.question('informa o sexo do professor (Digite F ou M): ', function (sexoProf) {
-                let generoProf = sexoProf
+                let generoProf = sexoProf.toUpperCase()
 
-                entradaDados.question('informe o nome do curso: ', function(nomeCurso){
+                entradaDados.question('informe o nome do curso: ', function (nomeCurso) {
                     let curso = nomeCurso
 
-                    entradaDados.question('informe o nome da disciplica: ', function(nomeDisciplina){
+                    entradaDados.question('informe o nome da disciplica: ', function (nomeDisciplina) {
                         let disciplina = nomeDisciplina
 
-                        entradaDados.question('nota 1: ', function(nota1){
+                        entradaDados.question('nota 1: ', function (nota1) {
                             let valor1 = nota1
 
-                            entradaDados.question('nota 2: ', function(nota2){
+                            entradaDados.question('nota 2: ', function (nota2) {
                                 let valor2 = nota2
 
-                                entradaDados.question('nota 3: ', function(nota3){
+                                entradaDados.question('nota 3: ', function (nota3) {
                                     let valor3 = nota3
 
-                                    entradaDados.question('nota 4: ', function(nota4){
+                                    entradaDados.question('nota 4: ', function (nota4) {
                                         let valor4 = nota4
                                         let resultado
 
-                                        if(
-                                            nomeAlun == '' 
-                                            || generoAlun == '' 
-                                            || nomeProf == '' 
-                                            || generoProf == '' 
-                                            || curso == '' 
-                                            || disciplina == '' 
-                                            || valor1 == '' 
-                                            || valor2 == '' 
+                                        if (
+                                            nomeAlun == ''
+                                            || generoAlun == ''
+                                            || nomeProfessor == ''
+                                            || generoProf == ''
+                                            || curso == ''
+                                            || disciplina == ''
+                                            || valor1 == ''
+                                            || valor2 == ''
                                             || valor3 == ''
                                             || valor4 == ''
-                                            ){
+                                        ) {
                                             console.log('Impossivel continuar operação, campos vazios')
-                                        } else if(isNaN(valor1) || isNaN(valor2) || isNaN(valor3) || isNaN(valor4)){
+                                        } else if (isNaN(valor1) || isNaN(valor2) || isNaN(valor3) || isNaN(valor4)) {
                                             console.log('Impossivel calcular, os valores não são numeros')
-
-                                        } else if(valor1 > 100 || valor2 > 100 || valor3 > 100 || valor4 > 100){
+                                            entradaDados.close()
+                                        } else if (valor1 > 100 || valor2 > 100 || valor3 > 100 || valor4 > 100) {
                                             console.log('Valor maior que o esperado, por favor digitar número máximo a 100')
-
-                                        } else if(valor1 < 0 || valor2 < 0 || valor3 < 0 || valor4 < 0){
+                                            entradaDados.close()
+                                        } else if (valor1 < 0 || valor2 < 0 || valor3 < 0 || valor4 < 0) {
                                             console.log('Número inferir a 0, por favor insira números máximos a 100')
+                                            entradaDados.close()
+                                        } else if (generoAlun != 'F' || generoAlun != 'M' || generoProf != 'F' || generoProf != 'M') {
+                                            console.log('expressão invalida, por favor digite o genero corretamente')
                                         } else {
                                             resultado = mediaResultado.media(nota1, nota2, nota3, nota4)
-                                            console.log(resultado)
-                                            if(resultado < 50){
-                                                // exibir reprovação ou form de reprovado
-                                            }else if(resultado >= 50 || resultado <= 69){
-                                                entradaDados.question('Aluno reprovado, por favor insira nota do exame: ', function(notaExame){
+                                            if (resultado >= 50 && resultado <= 69) {
+                                                entradaDados.question('Aluno reprovado, por favor insira nota do exame: ', function (notaExame) {
                                                     let notaDoExame = notaExame
-                                                    // chamar função de exam
+
+                                                    if (notaDoExame == '') {
+                                                        console.log('É necessario a nota do exame para calcular')
+                                                        entradaDados.close()
+                                                    } else {
+                                                        mediaResultado.formExame(nomeAlun, sexoAlun, nomeProf, sexoProf, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4, notaExame, resultado)
+                                                        entradaDados.close()
+                                                    }
                                                 })
-                                            } else if(mediaResultado.media >= 70){
-                                                //imprimir formulario
+                                            } else {
+                                                mediaResultado.form(nomeAlun, sexoAlun, nomeProf, sexoProf, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4, resultado)
+                                                entradaDados.close()
                                             }
                                         }
                                     })
@@ -86,6 +94,6 @@ entradaDados.question('Informe o nome do aluno(a): ', function (nomeAluno) {
                     })
                 })
             })
-        })    
+        })
     })
 })
--- exercicio-media/modulo/media.js
+++ exercicio-media/modulo/media.js
@@ -1,9 +1,8 @@
-function media(nota1, nota2, nota3, nota4){
+function media(nota1, nota2, nota3, nota4) {
     let valor1 = Number(nota1)
     let valor2 = Number(nota2)
     let valor3 = Number(nota3)
     let valor4 = Number(nota4)
-    let status = true
     let resultado
 
     resultado = (valor1 + valor2 + valor3 + valor4) / 4
@@ -11,24 +10,193 @@ function media(nota1, nota2, nota3, nota4){
 
 }
 
-function exame(notaExame) {
-    let notaDoExame = notaExame
-    let novoCalc = media.resultado
+function formExame(nomeAlun, sexoAlun, nomeProf, sexoProf, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4, notaExame, resultado) {
 
-    resultadoComExame = (notaDoExame + novoCalc) / 4
+    let nomeAluno = nomeAlun
+    let generoAlun = sexoAlun
+    let nomeProfessor = nomeProf
+    let generoProf = sexoProf
+    let curso = nomeCurso
+    let disciplina = nomeDisciplina
+    let valor1 = nota1
+    let valor2 = nota2
+    let valor3 = nota3
+    let valor4 = nota4
 
-    // fazer formulario proprio para exame aqui
-    
-    return resultadoComExame
-    
+    let notaDoExame = Number(notaExame)
+    let novoCalc = Number(resultado)
+    let resultadoComExame
+
+    resultadoComExame = (notaDoExame + novoCalc) / 2
+
+    if (resultadoComExame < 60) {
+        if (generoProf == 'F' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAluno} foi reprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaDoExame}\n
+            Média Final: ${novoCalc}
+            Média Final do exame: ${resultadoComExame}
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi reprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}< ${notaDoExame}\n
+            Média Final: ${novoCalc}\n
+            Média Final do exame: ${resultadoComExame}
+
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAlun} foi reprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaDoExame}\n
+            Média Final: ${novoCalc}\n
+            Média Final do exame: ${resultadoComExame}
+            `)
+        } else if (generoProf == 'F' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi reprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaDoExame}\n
+            Média Final: ${novoCalc}\n
+            Média Final do exame: ${resultadoComExame}
+            `)
+        }
+    } else if (resultadoComExame > 60) {
+        if (generoProf == 'F' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAlun} foi aprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaDoExame}\n
+            Média Final: ${novoCalc}\n
+            Média Final do exame: ${resultadoComExame}
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi aprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaDoExame}\n
+            Média Final: ${novoCalc}\n
+            Média Final do exame: ${resultadoComExame}
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAlun} foi aprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaDoExame}\n
+            Média Final: ${novoCalc}\n
+            Média Final do exame: ${resultadoComExame}
+            `)
+        } else if (generoProf == 'F' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi aprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaDoExame}\n
+            Média Final: ${novoCalc}\n
+            Média Final do exame: ${resultadoComExame}
+            `)
+        }
+    }
 }
 
-function form(nomeAlun, sexoAlun, nomeProfessor, generoProf, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4){
-    // fazer formulario usando true para pessoa aprovada e false para pessoa reprovada
+function form(nomeAlun, sexoAlun, nomeProf, sexoProf, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4, resultado) {
+
+    let nomeAluno = nomeAlun
+    let generoAlun = sexoAlun.toUpperCase()
+    let nomeProfessor = nomeProf
+    let generoProf = sexoProf.toUpperCase()
+    let curso = nomeCurso
+    let disciplina = nomeDisciplina
+    let valor1 = nota1
+    let valor2 = nota2
+    let valor3 = nota3
+    let valor4 = nota4
+    let mediaFinal = resultado
+
+
+    if (mediaFinal < 50) {
+        if (generoProf == 'F' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAluno} foi reprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi reprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAlun} foi reprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        } else if (generoProf == 'F' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi reprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        }
+    } else if (mediaFinal >= 70) {
+        if (generoProf == 'F' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAlun} foi aprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi aprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        } else if (generoProf == 'M' || generoAlun == 'F') {
+            return console.log(`
+            A aluna ${nomeAlun} foi aprovada na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professor: ${nomeProfessor}\n
+            Notas da aluna: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        } else if (generoProf == 'F' || generoAlun == 'M') {
+            return console.log(`
+            O aluno ${nomeAlun} foi aprovado na ${disciplina}. \n
+            Curso: ${curso} \n
+            Professora: ${nomeProfessor}\n
+            Notas do aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}\n
+            Média Final: ${mediaFinal}
+            `)
+        }
+    }
 }
 
 module.exports = {
     media,
     form,
-    exame
+    formExame,
 }
