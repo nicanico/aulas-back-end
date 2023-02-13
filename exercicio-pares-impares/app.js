e133bae2cc57f8f7aa33b37f4b1d82287412eebc
nicanico <nicsousan@gmail.com>
2023-02-12 23:51:17 -0300

    update 12/02 - exercício de números pares e impares, início dos callbacks e funções

---
+++ exercicio-pares-impares/app.js
@@ -0,0 +1,34 @@
+/*
+ * Objetivo: separar e exibir numeros pares e impares ao usuario
+ * Autor: Nicole
+ * Data: 10/02/23
+ * Versão: 1.0
+ */
+
+calculoParEImpar = require('./modulo/impar-par.js')
+
+var readline = require('readline')
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+entradaDados.question('Seja bem vindo(a) ao separador de números\n Insira o número inicial: ', function (inicialNumber) {
+    let numeroInicial = inicialNumber
+
+    entradaDados.question('Insira número final: ', function (finalNumber) {
+        let numeroFinal = finalNumber
+        let calcular
+
+        if (numeroInicial == '' || numeroFinal == '') {
+            console.log('Você deixou entradas vazias, tente novamente')
+            entradaDados.close()
+        } else if (isNaN(numeroInicial) || isNaN(numeroFinal)) {
+            console.log('A entrada não é um número, tente novamente')
+            entradaDados.close()
+        } else {
+            calcular = calculoParEImpar.exibirNumeros(inicialNumber, finalNumber)
+            entradaDados.close()
+        }
+    })
+})
---
+++ exercicio-pares-impares/modulo/impar-par.js
@@ -0,0 +1,22 @@
+/*
+ * Objetivo: separar e exibir numeros pares e impares ao usuario
+ * Autor: Nicole
+ * Data: 10/02/23
+ * Versão: 1.0
+ */
+
+function exibirNumeros(inicialNumber, finalNumber) {
+    console.log('estou funcionando')
+}
+
+function numerosImpares() {
+
+}
+
+function numerosPares() {
+
+}
+
+module.exports = {
+    exibirNumeros
+}
