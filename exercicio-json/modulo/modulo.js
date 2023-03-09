6954da026d29c24bafcb025a23618bdfce430ead
nicanico <nicsousan@gmail.com>
2023-03-09 00:32:22 -0300

    09/03 - update de novas funções feitas em aula

--- exercicio-json/modulo/modulo.js
+++ exercicio-json/modulo/modulo.js
@@ -6,8 +6,10 @@
  */
 // exportando a variavel 
 var estadosBrasil = require('./estados_cidades')
+const { estadosCidades } = require('./estados_cidades')
 
 const estadoCidade = estadosBrasil.estadosCidades.estados
+
 
 const getListaDeEstados = function (json) {
     // entrada
@@ -15,12 +17,12 @@ const getListaDeEstados = function (json) {
     const listEstadosJSON = {}
     const ufEstados = []
     let status
-    
+
     // processamento
-    if(estadosJSON != undefined){
-        estadosJSON.forEach(function(siglaEstados){
+    if (estadosJSON != undefined) {
+        estadosJSON.forEach(function (siglaEstados) {
             ufEstados.push(siglaEstados.sigla)
-            listEstadosJSON.uf = ufEstados
+            listEstadosJSON.UF = ufEstados
             listEstadosJSON.quantidade = ufEstados.length
         })
         status = true
@@ -29,21 +31,114 @@ const getListaDeEstados = function (json) {
     }
 
     // saida
-    if(status){
+    if (status) {
         console.log(listEstadosJSON)
         return listEstadosJSON
-    } else{
+    } else {
         return false
     }
 
 }
 
-const getDadosEstado = function (json, filtro){
+const getDadosEstado = function (json, filtro) {
     const uf = filtro
-    const estadosJSON = json
-    
+    const estadosJSON = json.slice()
+    const dadosEstado = {}
+    let status
+
+    if (uf != undefined) {
+        estadosJSON.forEach(function (estadosDados) {
+            if (estadosDados.sigla == uf) {
+                dadosEstado.uf = estadosDados.sigla
+                dadosEstado.descricao = estadosDados.nome
+                dadosEstado.capital = estadosDados.capital
+                dadosEstado.regiao = estadosDados.regiao
+
+            }
+        })
+
+        status = true
+
+    } else {
+        status = false
+    }
+
+    if (status) {
+        console.log(dadosEstado)
+        return dadosEstado
+    } else {
+        console.log('error')
+        return false
+    }
 
 }
 
-getListaDeEstados(estadoCidade)
-// getDadosEstado(estadoCidade, 'SP')
+const getCapitalEstado = function (json, filtro) {
+    const uf = filtro
+    const estadosJSON = json.slice()
+    const dadosEstadoCapital = {}
+    let status
+
+    if (uf != undefined) {
+        estadosJSON.forEach(function (dadosCapital) {
+            if (dadosCapital.sigla == uf) {
+                dadosEstadoCapital.uf = dadosCapital.sigla
+                dadosEstadoCapital.descricao = dadosCapital.nome
+                dadosEstadoCapital.capital = dadosCapital.capital
+            }
+        })
+        status = true
+
+    } else {
+        status = false
+    }
+
+    if (status) {
+
+        return dadosEstadoCapital
+    } else {
+        console.log('error')
+        return false
+    }
+
+}
+
+const getEstadosRegiao = function (json, filtro) {
+    const filtroRegiao = filtro
+    const estadosJSON = json.slice()
+    const dadosEstadoCapital = {}
+    const estado = []
+
+    let status
+
+    estadosJSON.forEach(function (dadosRegiao) {
+
+        if (dadosRegiao.regiao == filtroRegiao) {
+            dadosEstadoCapital.regiao = dadosRegiao.regiao
+
+            dadosEstadoCapital.estados = estado
+
+            const estadosDesUf = {}
+            // cria um id diferente para casa json com UF e descrição
+            estadosDesUf.uf = dadosRegiao.sigla
+            estadosDesUf.descricao = dadosRegiao.nome
+
+            estado.push(estadosDesUf)
+
+        }
+    })
+    console.log(dadosEstadoCapital)
+}
+
+const getCapitalPais = function (json) {
+    const estadosJSON = json.slice()
+}
+
+const getCidades = function (json, filtro) {
+
+}
+
+// getListaDeEstados(estadoCidade)
+// getDadosEstado(estadoCidade, 'RJ')
+// getCapitalEstado(estadoCidade, 'AC')
+getEstadosRegiao(estadoCidade, 'Sul')
